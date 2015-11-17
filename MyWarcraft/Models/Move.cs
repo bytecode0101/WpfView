d032c7c455ba8f82953e9320ceb297e7109e3194
andrei patrascu <Aluvspam@yahoo.com>
2015-11-17 21:05:00 +0200

    commit all

---
+++ MyWarcraft/Models/Move.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWarcraft.Models.Units;
+
+namespace MyWarcraft.Models
+{
+    public class Move : IOrder
+    {
+        public AbstractUnit Unit { get; set; }
+        public Point Target;
+        public void Execute()
+        {
+            Unit.Move(Target.x, Target.y);
+        }
+    }
+}
