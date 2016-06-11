9d8b3026a52d7e4d60875139098a3a4c4df981ce
ovidiu.szanto <bytecode0101@yahoo.com>
2016-06-11 13:23:52 +0300

    fixed namespaces

--- TilePathGame/Assets/MicroWarcraft/Utils/CommandExecutor.cs
+++ TilePathGame/Assets/MicroWarcraft/Utils/CommandExecutor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assets.MicroWarcraft.Models.Units
+namespace Assets.MicroWarcraft.Utils
 {
     public class CommandExecutor : ICommandExecutor
     {
